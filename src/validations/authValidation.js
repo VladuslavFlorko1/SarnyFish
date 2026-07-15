8d91c278875b443f4e783f9ed4a6a1e89502b7e1
VladuslavFlorko1 <florkovlad@gmail.com>
2026-07-15 23:01:00 +0300

    fix: allow spaces in username validation

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -1,5 +1,7 @@
 import { Joi, Segments } from 'celebrate';
 
+const usernamePattern = /^[a-zA-Z0-9_ ]+$/;
+
 export const loginSchema = {
   [Segments.BODY]: Joi.object({
     email: Joi.string().trim().email().required().lowercase().messages({
@@ -8,6 +10,7 @@ export const loginSchema = {
       'string.email': 'Email повинен бути дійсною електронною адресою',
       'any.required': 'Email є обовʼязковим',
     }),
+
     password: Joi.string().trim().min(8).max(20).required().messages({
       'string.base': 'Пароль має бути рядком',
       'string.empty': 'Пароль не може бути пустим',
@@ -20,16 +23,16 @@ export const loginSchema = {
 
 export const registerSchema = {
   [Segments.BODY]: Joi.object({
-    username: Joi.string().trim().required().min(3).max(18).pattern(/^[a-zA-Z0-9_]+$/).messages({
+    username: Joi.string().trim().required().min(3).max(18).pattern(usernamePattern).messages({
       'string.base': 'Імʼя користувача має бути рядком',
       'string.empty': 'Імʼя користувача не може бути пустим',
       'string.min': 'Імʼя користувача має бути не менше 3 символів',
       'string.max': 'Імʼя користувача має бути не більше 18 символів',
       'any.required': 'Імʼя користувача є обовʼязковим',
-      'string.pattern.base': 'Імʼя користувача може містити лише літери, цифри та підкреслення',
+      'string.pattern.base': 'Імʼя користувача може містити лише літери, цифри, пробіли та підкреслення',
     }),
 
-    email: Joi.string().trim().email().required().messages({
+    email: Joi.string().trim().email().required().lowercase().messages({
       'string.base': 'Email має бути рядком',
       'string.empty': 'Email не може бути пустим',
       'string.email': 'Email повинен бути дійсною електронною адресою',
@@ -44,11 +47,11 @@ export const registerSchema = {
       'any.required': 'Пароль є обовʼязковим',
     }),
   }),
-}
+};
 
 export const requestResetEmailSchema = {
   [Segments.BODY]: Joi.object({
-    email: Joi.string().email().required().messages({
+    email: Joi.string().trim().email().required().lowercase().messages({
       'string.base': 'Email має бути рядком',
       'string.empty': 'Email не може бути пустим',
       'string.email': 'Email повинен бути дійсною електронною адресою',
