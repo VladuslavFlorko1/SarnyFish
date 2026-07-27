7370a3edd7de9ab455211bebc7ebec1e7bd38d68
VladuslavFlorko1 <florkovlad@gmail.com>
2026-07-27 15:17:48 +0300

    feat: add GET /users/:id with relation status to current user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,51 @@
 import createHttpError from 'http-errors';
 import { User } from '../models/user.js';
+import { FriendRequest } from '../models/friendRequest.js';
 import { uploadToCloudinary } from '../services/uploadToCloudinary.js';
 
 export const getCurrentUser = async (req, res) => {
   res.status(200).json({ user: req.user });
+};
+
+export const getUserById = async (req, res) => {
+  const { id } = req.params;
+
+  const user = await User.findById(id);
+
+  if (!user) {
+    throw createHttpError(404, 'Користувача не знайдено');
+  }
+
+  let relationStatus = 'none';
+  let requestId = null;
+
+  const isFriend = req.user.friends.some((f) => f.toString() === id);
+
+  if (isFriend) {
+    relationStatus = 'friends';
+  } else {
+    const request = await FriendRequest.findOne({
+      status: 'pending',
+      $or: [
+        { from: req.user._id, to: id },
+        { from: id, to: req.user._id },
+      ],
+    });
+
+    if (request) {
+      requestId = request._id;
+      relationStatus =
+        request.from.toString() === req.user._id.toString()
+          ? 'pending_sent'
+          : 'pending_received';
+    }
+  }
+
+  res.status(200).json({
+    user,
+    relationStatus,
+    requestId,
+  });
 };
 
 export const updateUserAvatar = async (req, res) => {
@@ -11,22 +53,13 @@ export const updateUserAvatar = async (req, res) => {
     throw createHttpError(400, 'Файл аватара не надано');
   }
 
-  const result = await uploadToCloudinary(
-    req.file.buffer,
-    'avatars'
-  );
+  const result = await uploadToCloudinary(req.file.buffer, 'avatars');
 
   const user = await User.findByIdAndUpdate(
     req.user._id,
-    {
-      avatar: result.secure_url,
-    },
-    {
-      new: true,
-    }
+    { avatar: result.secure_url },
+    { new: true },
   );
 
-  res.status(200).json({
-    avatar: user.avatar,
-  });
+  res.status(200).json({ avatar: user.avatar });
 };
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,11 +1,36 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
+import { isValidObjectId } from 'mongoose';
 import { authenticate } from '../middlewares/authenticate.js';
-import { updateUserAvatar, getCurrentUser } from '../controllers/userController.js';
+import {
+  updateUserAvatar,
+  getCurrentUser,
+  getUserById,
+} from '../controllers/userController.js';
 import { upload } from '../middlewares/multer.js';
+
+const objectIdValidator = (value, helpers) => {
+  return isValidObjectId(value)
+    ? value
+    : helpers.message('Невірний формат ObjectId');
+};
+
+const userIdParamSchema = {
+  [Segments.PARAMS]: Joi.object({
+    id: Joi.string().custom(objectIdValidator).required(),
+  }),
+};
 
 const userRouter = Router();
 
 userRouter.get('/users/me', authenticate, getCurrentUser);
+
+userRouter.get(
+  '/users/:id',
+  authenticate,
+  celebrate(userIdParamSchema),
+  getUserById,
+);
 
 userRouter.patch(
   '/users/me/avatar',
