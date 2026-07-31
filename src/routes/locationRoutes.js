8f8d009994e105c50e264c01c2e95f48ac031f45
VladuslavFlorko1 <florkovlad@gmail.com>
2026-07-31 14:59:41 +0300

    fix: remove duplicate GET /locations routes

--- src/routes/locationRoutes.js
+++ src/routes/locationRoutes.js
@@ -9,23 +9,18 @@ import { optionalAuthenticate } from '../middlewares/optionalAuthenticate.js';
 
 const localRouter = Router();
 
-
-localRouter.get('/locations',celebrate(getLocationSchema), getLocations);
-
-localRouter.get('/locations/:id',celebrate(idValidationSchema), getLocationById);
-
-localRouter.post('/locations',authenticate,upload.array('images', 10),celebrate(locationSchema),createLocation,);
-
-localRouter.delete('/locations/:id',authenticate, celebrate(idValidationSchema), deleteLocation);
-
-localRouter.put('/locations/:id',authenticate, celebrate(idValidationSchema), updateLocation);
-
-localRouter.patch('/locations/:id', authenticate, celebrate(updateLocationSchema), patchLocation);
-
-localRouter.patch("/locations/:id/like", authenticate, celebrate(idValidationSchema), toggleLike);
-
 localRouter.get('/locations', optionalAuthenticate, celebrate(getLocationSchema), getLocations);
 
 localRouter.get('/locations/:id', optionalAuthenticate, celebrate(idValidationSchema), getLocationById);
+
+localRouter.post('/locations', authenticate, upload.array('images', 10), celebrate(locationSchema), createLocation);
+
+localRouter.delete('/locations/:id', authenticate, celebrate(idValidationSchema), deleteLocation);
+
+localRouter.put('/locations/:id', authenticate, celebrate(idValidationSchema), updateLocation);
+
+localRouter.patch('/locations/:id', authenticate, celebrate(updateLocationSchema), patchLocation);
+
+localRouter.patch('/locations/:id/like', authenticate, celebrate(idValidationSchema), toggleLike);
 
 export default localRouter;
